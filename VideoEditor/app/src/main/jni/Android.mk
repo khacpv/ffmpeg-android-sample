d8c7095a1fdb1839261896ada3313fb89f677176
khacpham <khacpv@gmail.com>
2015-12-09 14:56:19 +0700

    init project

---
+++ VideoEditor/app/src/main/jni/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := tutorial
+LOCAL_SRC_FILES := tutorial01.c
+LOCAL_LDLIBS := -llog -ljnigraphics -lz -landroid -ljnigraphics
+LOCAL_SHARED_LIBRARIES := libavformat libavcodec libswscale libavutil libavfilter libswresample
+LOCAL_LDFLAGS += -ljnigraphics
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,ffmpeg-2.8.3/android/arm)
---
+++ VideoEditor/app/src/main/obj/local/armeabi-v7a/objs-debug/tutorial01/tutorial01.o.d
@@ -0,0 +1,254 @@
+/Users/khacpham/Downloads/VideoEditor/app/src/main/obj/local/armeabi-v7a/objs-debug/tutorial01/tutorial01.o: \
+ /Users/khacpham/Downloads/VideoEditor/app/src/main/jni/tutorial01.c \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavcodec/avcodec.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/errno.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/samplefmt.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/avutil.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/common.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/inttypes.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/limits.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/math.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdio.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/string.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/alloca.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/strings.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/memory.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/attributes.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/version.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/macros.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/avconfig.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/mem.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/error.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/rational.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/mathematics.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/intfloat.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/log.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/pixfmt.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/old_pix_fmts.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/attributes.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/avutil.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/buffer.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/cpu.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/channel_layout.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/dict.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/frame.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/buffer.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/dict.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/samplefmt.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/log.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/pixfmt.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/rational.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavcodec/version.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/version.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavcodec/old_codec_ids.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavformat/avformat.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/time.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavformat/avio.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/common.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavformat/version.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libswscale/swscale.h \
+ /Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libswscale/version.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/wchar.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/jni.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/log.h \
+ /Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/bitmap.h
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavcodec/avcodec.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/errno.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/samplefmt.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/avutil.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/common.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/inttypes.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/math.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/strings.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/memory.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/attributes.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/version.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/macros.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/avconfig.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/mem.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/error.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/rational.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/mathematics.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/intfloat.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/log.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/pixfmt.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/old_pix_fmts.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/attributes.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/avutil.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/buffer.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/cpu.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/channel_layout.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/dict.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/frame.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/buffer.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/dict.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/samplefmt.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/log.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/pixfmt.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/rational.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavcodec/version.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/version.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavcodec/old_codec_ids.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavformat/avformat.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/time.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavformat/avio.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavutil/common.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libavformat/version.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libswscale/swscale.h:
+
+/Users/khacpham/Development/android-ndk-r10e/sources/ffmpeg-2.8.3/android/arm/include/libswscale/version.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/wchar.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+/Users/khacpham/Development/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/bitmap.h:
